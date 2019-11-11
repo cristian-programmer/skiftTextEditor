98da720f6d11023b67ccc6e27b8682575f80e043
Cristian <cvg97@misena.edu.co>
2019-11-11 02:44:43 -0500

    validations

--- kilo.c
+++ kilo.c
@@ -6,28 +6,41 @@ will you have to use q*/
 #include <stdlib.h>
 #include <termios.h>
 #include <ctype.h>
+#include <errno.h>
+
+#define KILL_KILO 'q'
+
+#define CTRL_KEY(k) ((k) & 0x1f)
 
 struct termios original_term;
 
+
+void die(const char *error){
+    perror(error);
+    exit(1);
+}
+
 void disableRawMode(){
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &original_term);
+    if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &original_term)== -1)
+        die("tcsetattr");
 }
 
 void enableRawMode(){   
-    tcgetattr(STDIN_FILENO, &original_term);
-
+    if(tcgetattr(STDIN_FILENO, &original_term)== -1)
+        die("tcgetattr");
+    atexit(disableRawMode); // is call where exit of the program
+    
     struct termios raw =  original_term;
     
-    tcgetattr(STDIN_FILENO, &raw);
     raw.c_cflag &= ~( BRKINT| ICRNL| ISTRIP | IXON );
     raw.c_cflag &= ~(OPOST);
     raw.c_cflag &= ~(CS8);
     raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
-    raw.c_cc[VMIN];
-    raw.c_cc[VTIME];
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+    raw.c_cc[VMIN] = 0;
+    raw.c_cc[VTIME] = 1;
+    if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1)
+     die("tcsetattr");
 
-    atexit(disableRawMode); // is call where exit of the program
 }
 
 
@@ -37,15 +50,19 @@ int main(){
 
     enableRawMode();
     
-    char c;
+    char c='\0';
+
     printf("STDIN_FILENO %i \n", STDIN_FILENO);
-    
-    while (read(STDIN_FILENO, &c, 1) == 1 && c!= 'q'){
+    if(read(STDIN_FILENO, &c, 1)==-1 && errno != EAGAIN) die("read");
+
+    while (1){
         if(iscntrl(c)) {
-            printf("%d\n", c);
+            printf("%d\r\n", c);
         }else {
-            printf("%d ('%c')\n", c, c);
+            printf("%d ('%c')\r\n", c, c);
         }
+
+        if(c == CTRL_KEY('q'))break;
     }
 
     return 0;
