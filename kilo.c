0aea300d57a4135487aee88eaf5e86050bf4ddca
Cristian <cvg97@misena.edu.co>
2019-11-11 01:05:32 -0500

    mode terminal enable/disable

---
+++ kilo.c
@@ -0,0 +1,47 @@
+/*to close program 
+will you have to use q*/
+
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <termios.h>
+#include <ctype.h>
+
+struct termios original_term;
+
+void disableRawMode(){
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &original_term);
+}
+
+void enableRawMode(){   
+    tcgetattr(STDIN_FILENO, &original_term);
+
+    struct termios raw =  original_term;
+    
+    tcgetattr(STDIN_FILENO, &raw);
+    raw.c_lflag &= ~(ECHO | ICANON);
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+
+    atexit(disableRawMode); // is call where exit of the program
+}
+
+
+
+int main(){
+    
+
+    enableRawMode();
+    
+    char c;
+    printf("STDIN_FILENO %i \n", STDIN_FILENO);
+    
+    while (read(STDIN_FILENO, &c, 1) == 1 && c!= 'q'){
+        if(iscntrl(c)) {
+            printf("%d\n", c);
+        }else {
+            printf("%d ('%c')\n", c, c);
+        }
+    }
+
+    return 0;
+}
---
+++ makefile
@@ -0,0 +1,2 @@
+kilo :	kilo.o
+	$(CC) kilo.c -o kilo -Wall -Wextra -pedantic -std=c99
